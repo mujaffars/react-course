74a842529fa6df6ef4540bbc6f968a3cbf14a3d3
Mujaffarkhan <mujaffarkhan.sanadi@clariontech.com>
2024-03-18 15:22:32 +0530

    Added components

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Component/Navbar';
+import Content from './Component/Content';
+
+function App() {
+  return (
+    <>
+      <h1></h1>
+      <Navbar thisprop="ABCD" />
+      <Content />
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Component/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div>
+      <div className="container">
+        <textarea></textarea>
+      </div>
+    </div>
+  )
+}
